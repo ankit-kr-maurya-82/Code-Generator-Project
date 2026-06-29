fddbe134e0577a1265549508617067d4778cc124
ankit-kr-maurya <ankitkrmaurya82@gmail.com>
2026-06-29 23:59:11 +0530

    Setup Application

--- main.py
+++ main.py
@@ -3,20 +3,3 @@ from  pydantic import BaseModel
 from typing import List
 
 app= FastAPI()
-
-
-class Tea(BaseModel):
-    id: int
-    name: str
-    origin: str
-
-teas : List[Tea]= []
-
-@app.get("/")
-def read_root():
-    return {"message":  "Welcome to chai aur code"}
-
-@app.get("/teas")
-def get_teas():
-    return teas
-
---
+++ static/js/app.js
@@ -0,0 +1,9 @@
+fetch("/generate", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+        prompt:prompt
+    })
+})
